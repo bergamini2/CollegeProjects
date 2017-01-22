3bcdcde878ad58217cf853a3f10f49264203788e
bergamini2 <nbergamini2@gmail.com>
2017-01-22 08:58:08 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -39,14 +39,14 @@ app.all('/*', function(req, res, next) {
 	next();
 });
 
-//console.log("Start of Program\n");
 // array of items that contain recycle
-var recycle = ["bottle", "can", "plastic", "paper", "aluminum", "steel", "glass", "water"];
+var recycle = ["bottle", "can", "plastic", "paper", "aluminum", "steel", "glass", "water",
+			  "coke", "soda", "pepsi", "cola", "recycle", "recycleable"];
 var tokenlist = [];
 
 	// params is a function that takes in a jpg image
 var params = {
-    images_file: fs.createReadStream('./toothpaste.jpg')
+    images_file: fs.createReadStream('./coke.jpg')
 };
 // process the params and has another function that takes in err and response
 visual_recognition.classify(params, function(err, res) {
@@ -56,33 +56,31 @@ visual_recognition.classify(params, function(err, res) {
   	//create an array of classList that contains all the classes name of the image
       var classlist = res.images[0].classifiers[0].classes.map(function(classObj){ return classObj.class; });
           for(var i = 0; i < res.images[0].classifiers[0].classes.length; i++) {
-              //console.log(classlist[i]);
-              //do we have to put it into tokenlist array.
+              //console.log(classlist[i]);     //tests what it is outputing for classes
               tokenlist.push(classlist[i]);
         }
-    //console.log(JSON.stringify(res, null, 2));
 });
 
 var x = 0;
 var result = [];
 var s = "Recycleable";
 setTimeout(function() {
-for (i = 0; i < recycle.length; i++) {
-	x++;
-    for (j = 0; j < tokenlist.length; j++) {
-        if(tokenlist[j].includes(recycle[i]) || recycle[i].includes(tokenlist[j])){
-       		//if tokenlist
-            result.push(s);
-            return;
-        }
+	for (i = 0; i < recycle.length; i++) {
+		x++;
+	    for (j = 0; j < tokenlist.length; j++) {
+	        if(tokenlist[j].includes(recycle[i]) || recycle[i].includes(tokenlist[j])){
+	       		//if tokenlist
+	            result.push(s);
+	            return;
+	        }
 
-    }
-    // if x == recycle.length, means that no item in the array was recycable
-    if( x == recycle.length){
-    	s = "Not Recycleable";
-    	result.push(s);
-    }
-}
+	    }
+	    // if x == recycle.length, means that no item in the array was recycable
+	    if( x == recycle.length){
+	    	s = "Not Recycleable";
+	    	result.push(s);
+	    }
+	}
 }, 5000)
 
 app.get('/', function(req, res) {
@@ -91,7 +89,3 @@ app.get('/', function(req, res) {
 app.server = http.createServer(app);
 app.server.listen(3000);
 
-
-
-
-//console.log("\nEnd of Program");
